15e04598675192dfa1c30c4d305dd37300caee0f
Dave Lu <davelu@yelp.com>
2019-07-25 23:15:40 -0700

    * Sword rotation * Cursor follow * Camera rotation

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,5 @@
 import 'phaser'
-import { Body } from 'matter-js'
+import { Composite } from 'matter-js'
 
 const WIDTH = 1000
 const HEIGHT = 1000
@@ -12,18 +12,21 @@ let WASD: {
   A: Phaser.Input.Keyboard.Key
   S: Phaser.Input.Keyboard.Key
   D: Phaser.Input.Keyboard.Key
+  Q: Phaser.Input.Keyboard.Key
+  E: Phaser.Input.Keyboard.Key
 }
 
-const DASH_SPEED = 0.01
+let pointer: Phaser.Physics.Matter.Image
+let player: Phaser.Physics.Matter.Image
+
+const DASH_SPEED = 8
 
 /* istanbul ignore next */
 function preload(this: { load: Phaser.Loader.LoaderPlugin }) {
   const { load } = this
 
-  load.setBaseURL('http://labs.phaser.io')
-
-  load.image('sword', 'assets/skies/deepblue.png')
-  load.image('player', 'assets/particles/red.png')
+  load.image('sword', '/assets/rapier.png')
+  load.image('player', 'http://labs.phaser.io/assets/particles/red.png')
 }
 
 /* istanbul ignore next */
@@ -34,23 +37,59 @@ function create(this: {
 }) {
   const { cameras, input, matter } = this
 
-  cameras.main.setBounds(-100, -100, WIDTH + 100, HEIGHT + 100)
-
   matter.world.setBounds(0, 0, WIDTH, HEIGHT)
 
-  matter.add.rectangle(100, 100, 20, 20, { isStatic: true })
-  matter.add.rectangle(200, 100, 20, 20, { isStatic: true })
-  matter.add.rectangle(300, 100, 20, 20, { isStatic: true })
-  matter.add.rectangle(100, 100, 20, 20, { isStatic: true })
-  matter.add.rectangle(100, 200, 20, 20, { isStatic: true })
-  matter.add.rectangle(100, 300, 20, 20, { isStatic: true })
+  matter.add.rectangle(200, 200, 10, 100, {})
 
-  const player = matter.add.image(80, 80, 'player').setScale(0.2, 0.2)
-  const sword = matter.add.rectangle(80, 80, 4, 80, {})
+  player = matter.add.image(10, 10, 'player').setScale(0.2, 0.2)
+  player.setDensity(1)
+  player.setFriction(1, 0.05)
 
-  matter.add.constraint(player, sword, 30, 0, {
+  const sword = matter.add.image(100, 100, 'sword').setScale(2, -2)
+  sword.setDensity(0.001)
+  sword.setFriction(1, 0.05)
+
+  pointer = matter.add.image(80, 80, 'player').setScale(0.08, 0.08)
+  pointer.setDensity(0.01)
+
+  // const player = Composite.create()
+  // Composite.add(player, player)
+  // Composite.add(player, sword)
+
+  const collisionGroups = {
+    terrain: 1,
+    players: matter.world.nextCategory(),
+    weapons: matter.world.nextCategory(),
+    controls: matter.world.nextCategory(),
+  }
+
+  player.setCollisionCategory(collisionGroups.players)
+  // player.setRotation(Math.PI)
+
+  sword.setCollisionCategory(collisionGroups.weapons)
+
+  pointer.setCollisionCategory(collisionGroups.controls)
+
+  player.setCollidesWith([collisionGroups.terrain, collisionGroups.players])
+  pointer.setCollidesWith([])
+
+  // pointer.setCollidesWith([])
+
+  // @ts-ignore
+  const leftHand = matter.add.constraint(player, sword, 40, 0.5, {
+    pointA: { x: -15, y: 0 },
+    pointB: { x: 0, y: -25 },
+  })
+  // @ts-ignore
+  const rightHand = matter.add.constraint(player, sword, 40, 0.5, {
+    pointA: { x: 15, y: 0 },
+    pointB: { x: 0, y: -35 },
+  })
+  // @ts-ignore
+  matter.add.constraint(pointer, sword, 1, 0.1, {
     pointA: { x: 0, y: 0 },
-    pointB: { x: 0, y: -40 },
+    pointB: { x: 0, y: 40 },
+    damping: 1,
   })
 
   cameras.main.startFollow(player)
@@ -62,27 +101,69 @@ function create(this: {
     A: Phaser.Input.Keyboard.KeyCodes.A,
     S: Phaser.Input.Keyboard.KeyCodes.S,
     D: Phaser.Input.Keyboard.KeyCodes.D,
+    Q: Phaser.Input.Keyboard.KeyCodes.Q,
+    E: Phaser.Input.Keyboard.KeyCodes.E,
   })
 
   WASD.W.on('down', () => {
-    player.setVelocity(0, 0)
+    // player.setVelocity(0, -DASH_SPEED)
+    // const dashForce = new Phaser.Math.Vector2().setToPolar(player.rotation, DASH_SPEED);
+    // console.log(dashForce.x, dashForce.y)
+    // player.applyForce(dashForce)
     player.applyForce(new Phaser.Math.Vector2(0, -DASH_SPEED))
   })
   WASD.A.on('down', () => {
-    player.setVelocity(0, 0)
+    // player.setVelocity(-DASH_SPEED, 0)
     player.applyForce(new Phaser.Math.Vector2(-DASH_SPEED, 0))
   })
   WASD.S.on('down', () => {
-    player.setVelocity(0, 0)
+    // player.setVelocity(0, DASH_SPEED)
     player.applyForce(new Phaser.Math.Vector2(0, DASH_SPEED))
   })
   WASD.D.on('down', () => {
-    player.setVelocity(0, 0)
+    // player.setVelocity(DASH_SPEED, 0)
     player.applyForce(new Phaser.Math.Vector2(DASH_SPEED, 0))
+  })
+  WASD.Q.on('down', () => {
+    player.setAngularVelocity(-0.01)
+  })
+  WASD.E.on('down', () => {
+    player.setAngularVelocity(0.01)
+  })
+
+  input.on('pointermove', () => {})
+  input.on('wheel', event => {
+    if (event.deltaY > 0) {
+      rightHand.pointA.y += 1
+      leftHand.pointA.y += 1
+    } else {
+      rightHand.pointA.y -= 1
+      leftHand.pointA.y -= 1
+    }
   })
 }
 
-function update() {}
+function update(this: {
+  cameras: Phaser.Cameras.Scene2D.CameraManager
+  input: Phaser.Input.InputPlugin
+}) {
+  const { cameras, input } = this
+
+  const playerPosition = new Phaser.Math.Vector2(player.x, player.y)
+  const cursorPosition = new Phaser.Math.Vector2(
+    input.activePointer.worldX,
+    input.activePointer.worldY
+  )
+  const orbitPosition = cursorPosition
+    .subtract(playerPosition)
+    .normalize()
+    .scale(110)
+    .add(playerPosition)
+  pointer.setPosition(orbitPosition.x, orbitPosition.y)
+
+  cameras.main.setRotation(-player.rotation)
+  console.log(player.rotation/Math.PI/2)
+}
 
 export const startGame = (type /* istanbul ignore next */ = Phaser.AUTO) => {
   new Phaser.Game({
